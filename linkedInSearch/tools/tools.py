6e0f74c490050c75f4926cd0e6275c23e3ac83a7
ForbidTANNEN <42479904+ForbidTANNEN@users.noreply.github.com>
2024-10-02 14:44:41 -0700

    Delete linkedInSearch directory

--- linkedInSearch/tools/tools.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
-
-def get_profile_url_tavily(name):
-    """Searches for Linkedin page"""
-    search = TavilySearchResults()
-    res = search.run(name)
-    return res
-
